0aead10d32f0e8e7177e77ed3d983bdeecc7ba4e
sohadutt <sohadutt@users.noreply.github.com>
2025-08-02 11:45:56 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
